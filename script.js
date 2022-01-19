3a8169bf1f3003ab0f6ac96ad3398f3e1b15551e
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-01-19 19:33:47 +0000

    completed black color for ETS

---
+++ script.js
@@ -0,0 +1,97 @@
+// const divEl = document.createElement('div');
+const container = document.querySelector('.container');
+const resetBtn = document.getElementById('reset-color');
+const eraseBtn = document.getElementById('erase-color');
+const colorPicker = document.getElementById('color-picker');
+let cpValue
+
+const dBlack = document.getElementById('black-color');
+
+const pixels = prompt('Enter Pixels');
+
+let col = pixels;
+let rows = pixels;
+
+createDivs(col,rows);
+
+// container.appendChild(divEl);
+
+// // add style to boxes
+// let boxWidth = parseInt(prompt('Enter number'));
+// let boxHeight = parseInt(prompt('Enter height number'));
+
+
+// let containerWidth = container.offsetWidth / boxWidth;
+// let containerHeight = container.offsetHeight / boxHeight;
+
+
+
+
+function createDivs(col, rows){
+
+    for (let i=0; i < (col * rows); i++){
+    const divEl = document.createElement('div');
+    container.style.gridTemplateColumns = `repeat(${col} , 1fr)`;
+    container.style.gridTemplateRows = `repeat(${rows} , 1fr)`;
+    divEl.classList.add('boxes');
+    // boxes.style.height = boxHeight+'px';
+    // boxes.style.width = boxWidth+'px';
+    container.appendChild(divEl);
+
+  };
+
+  blackColor(col, rows);
+};
+
+function blackColor(col, rows){
+  const boxes = document.querySelectorAll('.boxes');
+
+
+  for (let i=0; i < (col * rows); i++){
+    boxes[i].addEventListener('mouseover', ()=>{
+      boxes[i].style.backgroundColor = "black";
+    })
+  }
+}
+
+  resetBtn.addEventListener('click', ()=>{
+    const boxes1 = document.querySelectorAll('.boxes');
+    for (let i=0; i < (col * rows); i++){
+      boxes1[i].style.backgroundColor = "#fff";
+    }
+  });
+
+  eraseBtn.addEventListener('click', ()=>{
+    const boxes = document.querySelectorAll('.boxes');
+    for (let i=0; i < (col * rows); i++){
+      boxes[i].addEventListener('mouseover', ()=>{
+        boxes[i].style.backgroundColor = "#fff";
+      })
+    }
+  });
+
+  dBlack.addEventListener('click', ()=>{
+    const boxes = document.querySelectorAll('.boxes');
+    for (let i=0; i < (col * rows); i++){
+      boxes[i].addEventListener('mouseover', ()=>{
+        boxes[i].style.backgroundColor = "black";
+      })
+    }
+  } );
+
+  // Change color
+  colorPicker.onchange = ()=>{
+    cpValue = this.value;
+    const boxes = document.querySelectorAll('.boxes');
+    for (let i=0; i < (col * rows); i++){
+      boxes[i].addEventListener('mouseover', ()=>{
+        boxes[i].style.backgroundColor = cpValue;
+        console.log(boxes[i].style.backgroundColor);
+      })
+    }
+    console.log(cpValue);\
+    alert(cpValue);
+
+  }
+
+
